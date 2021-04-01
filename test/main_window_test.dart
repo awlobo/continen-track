9033102b4558a74f4c7822b02e8d739fe6fb158a
Andrea Lobo <andrea.lobo.arroyo@gmail.com>
2021-04-01 16:33:36 +0200

    Fix tests

--- test/main_window_test.dart
+++ test/main_window_test.dart
@@ -4,19 +4,9 @@ import 'package:continentracker/main.dart';
 
 void main() {
   testWidgets('Title is correct', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
     await tester.pumpWidget(ContinenTrackerApp());
-
-    // Verify that our counter starts at 0.
+    await tester.pumpAndSettle();
     expect(find.text('Continen-Tracker'), findsOneWidget);
     expect(find.text('ContinenTracker'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    // await tester.tap(find.byIcon(Icons.add));
-    // await tester.pump();
-
-    // Verify that our counter has incremented.
-    // expect(find.text('0'), findsNothing);
-    // expect(find.text('1'), findsOneWidget);
   });
 }
